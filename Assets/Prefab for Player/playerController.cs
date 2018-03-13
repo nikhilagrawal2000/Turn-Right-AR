26296b6d191126bffe4cc331320bcf46640184d5
nikhilagrawal2000 <nikhilagrawal1616@gmail.com>
2018-03-14 03:31:18 +0530

    Turn Right game with the supoort of AR

---
+++ Assets/Prefab for Player/playerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private AudioSource audi;
+    private int carState;
+    public float increaseRate;
+    private Vector3 ycarRotation;
+    public float decreaseRate;
+    public int mass;
+    public float drag;
+    public GameObject player;
+    public AudioClip Slipsound;
+    public AudioClip BackGroundsound;
+
+
+    [SerializeField]
+    private float freeVelocity;
+
+    private enum State
+    {
+        STRAIGHT, TURN, FREE
+    };
+
+    State current;
+
+    // Use this for initialization
+    void Start()
+    {
+        current = State.STRAIGHT;
+        ycarRotation = transform.localEulerAngles;
+        audi = GetComponent<AudioSource>();
+
+        audi.clip = BackGroundsound;
+        audi.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (current)
+        {
+
+            case State.STRAIGHT:
+                //Debug.Log("in straight");
+                if (ycarRotation.y > 0)
+                {
+                    ycarRotation.y = ycarRotation.y - decreaseRate * Time.deltaTime;
+                }
+                if (Input.GetMouseButtonDown(0))
+                {
+                    current = State.TURN;
+                }
+                transform.localRotation = Quaternion.Euler(ycarRotation);
+                if (transform.position.x < -100)
+                {
+                    current = State.FREE;
+                    transform.gameObject.AddComponent<Rigidbody>();
+                    Rigidbody rb = GetComponent<Rigidbody>();
+                    //transform.Translate(Vector3.forward*freeVelocity*Time.deltaTime);
+                    rb.velocity = transform.forward * freeVelocity;
+                    rb.mass = mass;
+                    rb.drag = drag;
+                }
+                break;
+
+            case State.TURN:
+               // Debug.Log("in turn");
+
+                if (Input.GetMouseButton(0))
+                {
+                    ycarRotation.y += increaseRate * Time.deltaTime;
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    current = State.STRAIGHT;
+                }
+                transform.localRotation = Quaternion.Euler(ycarRotation);
+               // Debug.Log(ycarRotation.y);
+                break;
+
+            case State.FREE:
+                //Debug.Log("FREE");
+                break;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "InnerBoundary")
+        {
+            audi.clip = Slipsound;
+            audi.Play();
+            current = State.FREE;
+            transform.gameObject.AddComponent<Rigidbody>();
+            Rigidbody rb = GetComponent<Rigidbody>();
+            //transform.Translate(Vector3.forward*freeVelocity*Time.deltaTime);
+            rb.velocity = transform.forward * freeVelocity;
+            rb.mass = mass;
+            rb.useGravity = false;
+            rb.drag = drag;
+            Debug.Log("Inner boundary child");
+            current = State.FREE;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "OuterBoundary")
+        {
+            if (audi.clip != Slipsound)
+            {
+                audi.clip = Slipsound;
+                audi.Play();
+            }
+            current = State.FREE;
+            Debug.Log("Outside");
+        }
+    }
+
+}
---
+++ Assets/Prefab for Player/playerControllerMain.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControllerMain : MonoBehaviour
+{
+    [SerializeField]
+    private float Drag;
+
+    [SerializeField]
+    private int Mass;
+
+    [SerializeField]
+    private float RotateSpeed = 2f;
+
+    [SerializeField]
+    private float initialRadius;
+
+    private float radius;
+
+    [SerializeField]
+    private float radiusDecreaseRate;
+
+    [SerializeField]
+    private float freeVelocity;
+
+    private Vector3 _centre;
+
+    private Vector3 centerToCar;
+    //private Vector3 offset;
+    private float _angle;
+
+    [SerializeField]
+    private float velocity;
+    private int state;
+
+    public GameObject player;
+    private Rigidbody playerRB;
+
+    public AudioClip CarRunnning;
+    private AudioSource audi;
+    // Use this for initialization
+    void Start()
+    {
+        audi = GetComponent<AudioSource>();
+        state = 0;
+        radius = initialRadius;
+        playerRB = GetComponent<Rigidbody>();
+        audi.clip = CarRunnning;
+        audi.Play();
+    }
+
+    // Update is called once per frame	
+    void Update()
+    {
+        switch (state)
+        {
+            case 0:
+                {
+                    transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        state = 1;
+                        radius = initialRadius;
+                        /*
+                            to set the coordinates of centre such that center is at a distance = radius from car and lies on the line
+                            joining car and transform.right
+                         */
+                        _centre = transform.position + ((transform.right) * radius);
+
+                        /* vector joining center and car */
+                        centerToCar = -_centre + transform.position;
+
+                        /* angle between carToCenter and -ve z axis */
+                        if (centerToCar.z >= 0)
+                        {
+                            _angle = Mathf.Acos((centerToCar.x) / radius);
+                        }
+                        else
+                        {
+                            _angle = 2 * Mathf.PI - Mathf.Acos((centerToCar.x) / radius);
+                        }
+
+                    }
+                    if (transform.GetChild(0).position.x < -100)
+                    {
+                        state = 2;
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    radius -= Time.deltaTime * radiusDecreaseRate;
+                    Vector3 vec = _centre - transform.position;
+                    transform.right = vec / radius;
+
+                    _angle = _angle - RotateSpeed * Time.deltaTime;
+                    Vector3 offset = new Vector3(Mathf.Cos(_angle), 0, Mathf.Sin(_angle)) * radius;
+                    transform.position = _centre + offset;
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        state = 0;
+                    }
+                    if (transform.GetChild(0).position.x < -100)
+                    {
+                        state = 2;
+                    }
+                    break;
+                }
+            case 2:
+                //transform.Translate(Vector3.forward * freeVelocity * Time.deltaTime);
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "InnerBoundary")
+        {
+            Debug.Log("Inner boundary");
+            state = 2;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "OuterBoundary")
+        {
+            state = 2;
+            playerRB.velocity = transform.forward * freeVelocity;
+            playerRB.mass = Mass;
+            playerRB.drag = Drag;
+            Debug.Log("Outside");
+            audi.Stop();
+        
+        }
+    }
+}
